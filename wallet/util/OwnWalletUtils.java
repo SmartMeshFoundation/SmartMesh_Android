5b68706ab57970da2c1ed3b4d657530a5be0515a
SmartMesh <32663487+SmartMeshFoundation@users.noreply.github.com>
2017-11-09 04:35:31 -0600

    Update OwnWalletUtils.java

--- wallet/util/OwnWalletUtils.java
+++ wallet/util/OwnWalletUtils.java
@@ -17,11 +17,6 @@ public class OwnWalletUtils extends WalletUtils {
 
     // OVERRIDING THOSE METHODS BECAUSE OF CUSTOM WALLET NAMING (CUTING ALL THE TIMESTAMPTS FOR INTERNAL STORAGE)
 
-    /**
-     * 创建一个完整的钱包
-     * @param password 密码
-     * @param destinationDirectory  文件内容
-     * */
     public static String generateFullNewWalletFile(String password, File destinationDirectory)
             throws NoSuchAlgorithmException, NoSuchProviderException,
             InvalidAlgorithmParameterException, CipherException, IOException {
@@ -29,23 +24,13 @@ public class OwnWalletUtils extends WalletUtils {
         return generateNewWalletFile(password, destinationDirectory, true);
     }
 
-    /**
-     * 创建一个轻型的钱包 （比完整钱包快）
-     * @param password 密码
-     * @param destinationDirectory  文件内容
-     * */
+
     public static String generateLightNewWalletFile(String password, File destinationDirectory)
             throws NoSuchAlgorithmException, NoSuchProviderException,
             InvalidAlgorithmParameterException, CipherException, IOException {
 
         return generateNewWalletFile(password, destinationDirectory, false);
     }
-
-    /**
-     * 创建钱包
-     * @param password 密码
-     * @param useFullScrypt 是否生成完整的钱包
-     * */
     public static WalletFile generateNewWalletFile(String password, boolean useFullScrypt)
             throws CipherException, IOException, InvalidAlgorithmParameterException,
             NoSuchAlgorithmException, NoSuchProviderException {
@@ -53,26 +38,12 @@ public class OwnWalletUtils extends WalletUtils {
         ECKeyPair ecKeyPair = Keys.createEcKeyPair();
         return generateWalletFile(password, ecKeyPair, useFullScrypt);
     }
-    /**
-     * 导入钱包
-     * @param password 密码
-     * @param keys 秘钥对
-     * @param useFullScrypt 是否生成完整的钱包
-     * */
     public static WalletFile importNewWalletFile(String password,ECKeyPair keys, boolean useFullScrypt)
             throws CipherException, IOException, InvalidAlgorithmParameterException,
             NoSuchAlgorithmException, NoSuchProviderException {
 
         return generateWalletFile(password, keys, useFullScrypt);
     }
-
-    /**
-     * 导入钱包
-     * @param password 密码
-     * @param ecKeyPair 秘钥对
-     * @param useFullScrypt 是否生成完整的钱包
-     * @return 钱包地址
-     * */
     public static WalletFile generateWalletFile(String password, ECKeyPair ecKeyPair, boolean useFullScrypt)
             throws CipherException, IOException {
 
