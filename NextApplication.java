f3e6e992a6842b85a82927de8f24a70ae7339947
Limit <563150732@qq.com>
2018-01-08 17:16:13 +0800

    Repair avatar repeat the problem

--- NextApplication.java
+++ NextApplication.java
@@ -2,8 +2,6 @@ package com.lingtuan.firefly;
 
 import android.app.Application;
 import android.content.Context;
-import android.content.Intent;
-import android.content.res.AssetManager;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
@@ -12,14 +10,11 @@ import android.widget.ImageView;
 
 import com.lingtuan.firefly.custom.BitmapFillet;
 import com.lingtuan.firefly.custom.RoundBitmapDisplayer;
-import com.lingtuan.firefly.listener.RequestListener;
-import com.lingtuan.firefly.util.Constants;
 import com.lingtuan.firefly.util.CrashHandler;
-import com.lingtuan.firefly.util.MySharedPrefs;
 import com.lingtuan.firefly.util.SDCardCtrl;
 import com.lingtuan.firefly.util.SmileyParser;
 import com.lingtuan.firefly.util.Utils;
-import com.lingtuan.firefly.util.netutil.NetRequestImpl;
+import com.lingtuan.firefly.util.netutil.NetRequestUtils;
 import com.lingtuan.firefly.vo.UserInfoVo;
 import com.lingtuan.firefly.xmpp.XmppUtils;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
@@ -30,38 +25,16 @@ import com.nostra13.universalimageloader.core.assist.ImageSize;
 import com.nostra13.universalimageloader.core.imageaware.ImageViewAware;
 import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.util.Timer;
-import java.util.TimerTask;
-
-import geth.EthereumClient;
-import geth.Geth;
-import geth.Header;
-import geth.NewHeadHandler;
-import geth.Node;
-import geth.NodeConfig;
-import geth.SyncProgress;
 
 
 /**
  * Created  on 2017/8/18.
  */
 
-public class NextApplication extends Application implements NewHeadHandler {
+public class NextApplication extends Application{
 
     public static Context mContext;
     public static UserInfoVo myInfo;
-    public static EthereumClient ec;
 
     public static ImageLoader mImageLoader = null;
     private static DisplayImageOptions mOptionsNull;
@@ -70,11 +43,6 @@ public class NextApplication extends Application implements NewHeadHandler {
     /*expression*/
     public static SmileyParser mSmileyParser;
 
-    private long totalNum = 2023108;
-    private boolean hasGetTotalNum;
-    private long currentNum;
-    private Timer timer;
-    private TimerTask task;
     @Override
     public void onCreate() {
         super.onCreate();
@@ -84,8 +52,6 @@ public class NextApplication extends Application implements NewHeadHandler {
 
         XmppUtils.isLogining = false;
         mContext = this;
-        /**init Ethereum Client*/
-        initEthereumClient();
         try {
             myInfo = new UserInfoVo().readMyUserInfo(this);
             Resources res = getResources();
@@ -96,6 +62,8 @@ public class NextApplication extends Application implements NewHeadHandler {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        NetRequestUtils.getInstance().getServerTime(this);
 
         initImageLoaderConfig();
 
@@ -108,108 +76,10 @@ public class NextApplication extends Application implements NewHeadHandler {
         crashHandler.init(getApplicationContext());
     }
 
-    /**
-     * Initialize ethereum client
-     * */
-    private void initEthereumClient(){
-        try {
-            int state = MySharedPrefs.readInt(this, MySharedPrefs.FILE_USER, MySharedPrefs.AGREE_SYNC_BLOCK);
-            if(state != 0) {
-                startSync(state);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * start sync node method
-     * @param state state  0 no sync  1 syncing  2 complete synchronously
-     * */
-    public void startSync(final int state){
-        NetRequestImpl.getInstance().getSyncNode(new RequestListener() {
-            @Override
-            public void start() {
-
-            }
-
-            @Override
-            public void success(JSONObject response) {
-                try {
-                    JSONArray array = response.optJSONArray("data");
-                    writeAsset(array);
-                    startNode(state);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    startNode(state);
-                }
-
-            }
-
-            @Override
-            public void error(int errorCode, String errorMsg) {
-                startNode(state);
-            }
-        });
-    }
-
-    /**
-     * write to file
-     * @param array  JsonArray file
-     * */
-    public void writeAsset(JSONArray array) throws IOException {
-        File dir = new File(getFilesDir() + SDCardCtrl.DROIDPATH);
-        if (!dir.exists()) {
-            dir.mkdirs();
-        }
-        File f = new File(dir, "static-nodes.json");
-        if (!f.exists()) {
-            f.createNewFile();
-        }
-        copyString(array.toString(), f);
-    }
-
-    /**
-     * copy string
-     * @param fileContents file content
-     * @param outputFile out put file
-     * */
-    public void copyString(String fileContents, File outputFile) throws FileNotFoundException {
-        OutputStream output = new FileOutputStream(outputFile);
-        PrintWriter p = new PrintWriter(output);
-        p.println(fileContents);
-        p.flush();
-        p.close();
-    }
-
-    /**
-     * start sync node
-     * @param state  0 no sync  1 syncing  2 complete synchronously
-     * */
-    private void startNode(int state){
-        try {
-            NodeConfig nodeConfig = Geth.newNodeConfig();
-            if (!Constants.GLOBAL_SWITCH_OPEN){
-                nodeConfig.setEthereumDatabaseCache(16);
-                nodeConfig.setEthereumEnabled(true);
-                nodeConfig.setEthereumGenesis(Geth.testnetGenesis());
-                nodeConfig.setEthereumNetworkID(3);
-            }
-            Node node = Geth.newNode(getFilesDir() + SDCardCtrl.ETHEREUM ,nodeConfig);
-            node.start();
-            ec = node.getEthereumClient();
-            if(state ==1){
-                ec.subscribeNewHead(new geth.Context(),this,16);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
 
     private void initImageLoaderConfig() {
         int width = getResources().getDisplayMetrics().widthPixels;
-        int height = getResources().getDisplayMetrics().heightPixels;
+        int  height = getResources().getDisplayMetrics().heightPixels;
 
         /** Use the default configuration*/
         ImageLoaderConfiguration configuration = new ImageLoaderConfiguration.Builder(getApplicationContext())
@@ -222,9 +92,9 @@ public class NextApplication extends Application implements NewHeadHandler {
 
         mOptionsCircle = new DisplayImageOptions.Builder()
                 .bitmapConfig(Bitmap.Config.RGB_565)
-                .showImageForEmptyUri(R.drawable.icon_default_avater)
-                .showImageOnFail(R.drawable.icon_default_avater)
-                .showImageOnLoading(R.drawable.icon_default_avater)
+                .showImageForEmptyUri(R.drawable.nothing)
+                .showImageOnFail(R.drawable.nothing)
+                .showImageOnLoading(R.drawable.nothing)
                 .cacheOnDisc(true)
                 .cacheInMemory(true)
                 .displayer(new RoundBitmapDisplayer(BitmapFillet.ROUND, 0))
@@ -242,7 +112,6 @@ public class NextApplication extends Application implements NewHeadHandler {
                 .build();
 
     }
-
 
     /*According to circular head*/
     public static void displayCircleImage(ImageView avatarView, String avatarUrl) {
@@ -278,56 +147,5 @@ public class NextApplication extends Application implements NewHeadHandler {
         Utils.settingLanguage(NextApplication.this);
     }
 
-
-    private  void  getNumber(){
-        long newValue = 0;
-        try {
-            newValue = NextApplication.ec.getHeaderByNumber(new geth.Context(),-1).getNumber();
-            if(!hasGetTotalNum){
-                SyncProgress syncProgress = NextApplication.ec.syncProgress(new geth.Context());
-                if(syncProgress!=null){
-                    hasGetTotalNum = true;
-                    totalNum = syncProgress.getHighestBlock();
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        if(newValue<=currentNum){
-            return;
-        }
-        currentNum = newValue;
-        if( currentNum>=totalNum){
-            MySharedPrefs.writeInt(mContext,MySharedPrefs.FILE_USER,MySharedPrefs.AGREE_SYNC_BLOCK,2);
-            if(timer!=null){
-                timer.cancel();
-            }
-        }
-        Intent intent = new Intent(Constants.SYNC_PROGRESS);
-        intent.putExtra("currentNumber",currentNum);
-        intent.putExtra("totalNumber",totalNum);
-        Utils.sendBroadcastReceiver(mContext,intent,false);
-    }
-
-
-    @Override
-    public void onError(String s) {
-
-    }
-
-    @Override
-    public void onNewHead(Header header) {
-        if(timer == null){
-            timer = new Timer();
-            task = new TimerTask() {
-                @Override
-                public void run() {
-                    getNumber();
-                }
-            };
-            timer.schedule(task,0,2000);
-        }
-    }
 }
 
