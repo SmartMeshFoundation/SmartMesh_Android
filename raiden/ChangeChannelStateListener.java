300f8c6aa05016a780464cfba2bf19dd387443fd
Rick1219 <16053737+wkp91@users.noreply.github.com>
2018-02-15 18:11:18 +0800

    ChangeChannelStateListener.java
    raiden channel listener

---
+++ raiden/ChangeChannelStateListener.java
@@ -0,0 +1,27 @@
+package com.lingtuan.firefly.raiden;
+
+/**
+ * Created on 2018/2/15.
+ */
+
+public interface ChangeChannelStateListener {
+
+    /**
+     * change channel
+     * @param position  list position
+     * @param isOpen    true open false  close
+     */
+    void changeChannel(int position,boolean isOpen);
+
+    /**
+     * deposit channel
+     * @param position list position
+     * */
+    void deopsitChannel(int position);
+
+    /**
+     * transfer channel
+     * @param position list position
+     * */
+    void transferChannel(int position);
+}
