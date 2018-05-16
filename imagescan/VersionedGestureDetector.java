588890b11e7bee971ec2a44ec2ac5c3c58d5a7df
Limit <563150732@qq.com>
2018-05-16 17:33:26 +0800

    VersionedGestureDetector.java

--- imagescan/VersionedGestureDetector.java
+++ imagescan/VersionedGestureDetector.java
@@ -114,7 +114,7 @@ public abstract class VersionedGestureDetector {
 				if (!mIsDragging) {
 					// Use Pythagoras to see if drag length is larger than
 					// touch slop
-					mIsDragging = FloatMath.sqrt((dx * dx) + (dy * dy)) >= mTouchSlop;
+					mIsDragging = Math.sqrt((dx * dx) + (dy * dy)) >= mTouchSlop;
 				}
 
 				if (mIsDragging) {
