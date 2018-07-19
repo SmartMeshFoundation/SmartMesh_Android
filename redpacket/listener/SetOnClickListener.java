a783f1e30e449cbc763f52c911e8168d1d4d3438
Limit <563150732@qq.com>
2018-07-19 18:04:58 +0800

    red packet listener

---
+++ redpacket/listener/SetOnClickListener.java
@@ -0,0 +1,9 @@
+package com.lingtuan.firefly.redpacket.listener;
+
+/**
+ * set on click listener
+ * item点击事件
+ * */
+public interface SetOnClickListener {
+    void onItemClickListener(int position);
+}
