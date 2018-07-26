11d794a6e10faf560770bf1815726be0b2373cd1
Limit <563150732@qq.com>
2018-07-26 13:43:39 +0800

    update message list

--- message/MsgTransListUI.java
+++ message/MsgTransListUI.java
@@ -1,53 +1,49 @@
 package com.lingtuan.firefly.message;
 
 import android.content.BroadcastReceiver;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.IBinder;
 import android.os.Message;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v4.widget.SwipeRefreshLayout.OnRefreshListener;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.ListView;
 
 import com.lingtuan.firefly.R;
 import com.lingtuan.firefly.adapter.MsgTransAdapter;
 import com.lingtuan.firefly.base.BaseActivity;
-import com.lingtuan.firefly.contact.adapter.ContactAddAdapter;
 import com.lingtuan.firefly.db.user.FinalUserDataBase;
-import com.lingtuan.firefly.offline.AppNetService;
-import com.lingtuan.firefly.offline.vo.WifiPeopleVO;
 import com.lingtuan.firefly.util.MyDialogFragment;
 import com.lingtuan.firefly.util.MyToast;
 import com.lingtuan.firefly.util.Utils;
 import com.lingtuan.firefly.vo.ChatMsg;
-import com.lingtuan.firefly.vo.UserBaseVo;
 import com.lingtuan.firefly.wallet.TransactionDetailActivity;
-import com.lingtuan.firefly.wallet.WalletSendActivity;
 import com.lingtuan.firefly.wallet.vo.TransVo;
 import com.lingtuan.firefly.xmpp.XmppAction;
 
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.OnItemClick;
+import butterknife.OnItemLongClick;
+
 /**
  * Add buddy list page
  */
-public class MsgTransListUI extends BaseActivity implements OnItemClickListener, OnRefreshListener, OnItemLongClickListener{
+public class MsgTransListUI extends BaseActivity implements  OnRefreshListener{
 
-	private ListView listView;
+	@BindView(R.id.refreshListView)
+	ListView listView;
+	@BindView(R.id.swipe_container)
+	SwipeRefreshLayout swipeLayout;
+
 	private List<ChatMsg> mList;
 	private MsgTransAdapter mAdapter;
-
 	private String chatId;
-	private SwipeRefreshLayout swipeLayout;
 	private TransMsgRecriver transMsgRecriver;
 
 	@Override
@@ -57,16 +53,12 @@ public class MsgTransListUI extends BaseActivity implements OnItemClickListener,
 
 	@Override
 	protected void findViewById() {
-		listView = (ListView) findViewById(R.id.refreshListView);
 		chatId = getIntent().getExtras().getString("chatid");
-		swipeLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_container);
 	}
 
 	@Override
 	protected void setListener() {
 		swipeLayout.setOnRefreshListener(this);
-		listView.setOnItemClickListener(this);
-		listView.setOnItemLongClickListener(this);
 	}
 
 	@Override
@@ -89,30 +81,6 @@ public class MsgTransListUI extends BaseActivity implements OnItemClickListener,
     	}
     }
 
-	@Override
-	public void onItemClick(AdapterView<?> parent, View view, int position,long id) {
-		Intent intent = new Intent(MsgTransListUI.this,TransactionDetailActivity.class);
-		ChatMsg chatmsg = mList.get(position);
-		TransVo transVo = new TransVo();
-		transVo.setTime(chatmsg.getCreateTime());
-		transVo.setType(Integer.parseInt(chatmsg.getMode()));
-		transVo.setTxBlockNumber(Integer.parseInt(chatmsg.getTxBlockNumber()));
-		transVo.setFee(chatmsg.getFee());
-		transVo.setValue(chatmsg.getMoney());
-		transVo.setTxurl(chatmsg.getShareUrl());
-		transVo.setTx(chatmsg.getNumber());
-		transVo.setFromAddress(chatmsg.getFromAddress());
-		transVo.setToAddress(chatmsg.getToAddress());
-		transVo.setState(chatmsg.getInviteType() + 1);
-		transVo.setNoticeType(chatmsg.getNoticeType());
-		transVo.setTokenAddress(chatmsg.getTokenAddress());
-		transVo.setName(chatmsg.getTokenName());
-		transVo.setSymbol(chatmsg.getTokenSymbol());
-		transVo.setLogo(chatmsg.getTokenLogo());
-		intent.putExtra("transVo",transVo);
-		startActivity(intent);
-		Utils.openNewActivityAnim(MsgTransListUI.this, false);
-	}
 
 	@Override
 	public void onRefresh() {
@@ -160,7 +128,33 @@ public class MsgTransListUI extends BaseActivity implements OnItemClickListener,
 		}
 		
 	}
-	@Override
+
+	@OnItemClick(R.id.refreshListView)
+	void onItemClick(AdapterView<?> parent, View view, int position,long id) {
+		Intent intent = new Intent(MsgTransListUI.this,TransactionDetailActivity.class);
+		ChatMsg chatmsg = mList.get(position);
+		TransVo transVo = new TransVo();
+		transVo.setTime(chatmsg.getCreateTime());
+		transVo.setType(Integer.parseInt(chatmsg.getMode()));
+		transVo.setTxBlockNumber(Integer.parseInt(chatmsg.getTxBlockNumber()));
+		transVo.setFee(chatmsg.getFee());
+		transVo.setValue(chatmsg.getMoney());
+		transVo.setTxurl(chatmsg.getShareUrl());
+		transVo.setTx(chatmsg.getNumber());
+		transVo.setFromAddress(chatmsg.getFromAddress());
+		transVo.setToAddress(chatmsg.getToAddress());
+		transVo.setState(chatmsg.getInviteType() + 1);
+		transVo.setNoticeType(chatmsg.getNoticeType());
+		transVo.setTokenAddress(chatmsg.getTokenAddress());
+		transVo.setName(chatmsg.getTokenName());
+		transVo.setSymbol(chatmsg.getTokenSymbol());
+		transVo.setLogo(chatmsg.getTokenLogo());
+		intent.putExtra("transVo",transVo);
+		startActivity(intent);
+		Utils.openNewActivityAnim(MsgTransListUI.this, false);
+	}
+
+	@OnItemLongClick(R.id.refreshListView)
 	public boolean onItemLongClick(AdapterView<?> parent, View view,final int position, long id) {
 		MyDialogFragment mdf = new MyDialogFragment(MyDialogFragment.DIALOG_LIST,R.array.delete_trans_array);
 		mdf.setItemClickCallback(new MyDialogFragment.ItemClickCallback() {
