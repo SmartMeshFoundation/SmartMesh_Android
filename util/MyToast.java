7089181a45536c8b777dd88c5aa1d31ca04f1394
limit <limit@smartmesh.io>
2018-09-21 17:35:07 +0800

    update toast

--- util/MyToast.java
+++ util/MyToast.java
@@ -1,6 +1,7 @@
 package com.lingtuan.firefly.util;
 import android.content.Context;
 import android.text.TextUtils;
+import android.view.Gravity;
 import android.widget.Toast;
 
 public class MyToast {
@@ -14,10 +15,32 @@ public class MyToast {
 			}
 			if (context != null) {
 				if (toast == null) {
-					toast = Toast.makeText(context, message, Toast.LENGTH_SHORT);
+					toast = Toast.makeText(context, message, Toast.LENGTH_LONG);
 				} else {
 					toast.setText(message);
 				}
+				toast.setGravity(Gravity.BOTTOM, 0, 250);
+				toast.show();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}catch(Error e){
+			e.printStackTrace();
+		}
+	}
+
+	public static void showCenterToast(Context context, String message) {
+		try {
+			if(TextUtils.isEmpty(message)){
+				return;
+			}
+			if (context != null) {
+				if (toast == null) {
+					toast = Toast.makeText(context, message, Toast.LENGTH_LONG);
+				} else {
+					toast.setText(message);
+				}
+				toast.setGravity(Gravity.CENTER, 0, 0);
 				toast.show();
 			}
 		} catch (Exception e) {
