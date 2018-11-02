2f7f79df41f58cc9730b4dec5760656ee23edfe0
limit <limit@smartmesh.io>
2018-11-02 17:50:20 +0800

    photon type

---
+++ raiden/RaidenStatusType.java
@@ -0,0 +1,9 @@
+package com.lingtuan.firefly.raiden;
+
+public class RaidenStatusType {
+    public static int DISCONNECTED = 0;
+    public static int Connected = 1;
+    public static int Closed = 2;
+    public static int Reconnecting = 3;
+    public static int Default = -999;
+}
